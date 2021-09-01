8e36a67ff4c5d74a7e67162bb0cef0e6f0f0e131
yayanyang <yayanyang@gmail.com>
2021-09-01 10:19:46 +0800

    update

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/web3zerotrust/signer
+
+go 1.17
+
+require github.com/libs4go/ethers v0.0.0-20210830141338-f7af7197479f
+
+require (
+	github.com/libs4go/crypto v0.0.0-20210723035624-62aa97055ac2 // indirect
+	github.com/libs4go/encoding v0.0.0-20210720054946-fe0a4a6f4c7a // indirect
+	github.com/libs4go/errors v0.0.3 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
+
+
+replace github.com/libs4go/ethers => ../ethers
---
+++ signer.go
@@ -0,0 +1,25 @@
+package signer
+
+import (
+	"context"
+
+	ethersigner "github.com/libs4go/ethers/signer"
+)
+
+// TrustClient trust client context
+type TrustClient struct {
+	ID    string // client id
+	Token string // client session token
+}
+
+type EthersSigner interface {
+	// Ether wallet receive address
+	Addresss(ctx context.Context, client *TrustClient) string
+	// SignTypedData implement eip712 sign ...
+	SignTypedData(ctx context.Context, client *TrustClient, typedData *ethersigner.TypedData) ([]byte, error)
+	// SignTransaction sign ether transaction
+	SignTransaction(ctx context.Context, client *TrustClient, tx *ethersigner.Transaction) error
+}
+
+type KeyStore interface {
+}
