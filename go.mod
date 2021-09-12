c180375398f18e1ea8e623ca68e72b75e40792f0
yayanyang <yayanyang@gmail.com>
2021-09-12 10:01:11 +0800

    update

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/web3zerotrust/signer
 go 1.17
 
 require (
+	github.com/bwmarrin/snowflake v0.3.0
 	github.com/libs4go/crypto v0.0.1
 	github.com/libs4go/encoding v0.0.0-20210720054946-fe0a4a6f4c7a
 	github.com/libs4go/errors v0.0.3
--- signer.go
+++ signer.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"crypto/elliptic"
 	"math/big"
+	"time"
 
 	"github.com/libs4go/errors"
+	"github.com/libs4go/ethers/signer"
 )
 
 // ScopeOfAPIError .
@@ -16,20 +18,57 @@ var (
 	ErrNotFound = errors.New("Resource not found", errors.WithVendor(errVendor), errors.WithCode(-1))
 )
 
-// // TrustClient trust client context
-// type TrustClient struct {
-// 	ID    string // client id
-// 	Token string // client session token
-// }
+type EthersSigner interface {
+	// Ether wallet receive address
+	Addresss(ctx context.Context, userID string) string
+	// SignTypedData implement eip712 sign ...
+	SignTypedData(ctx context.Context, userID string, typedData *signer.TypedData) ([]byte, error)
+	// SignTransaction sign ether transaction
+	SignTransaction(ctx context.Context, userID string, tx *signer.Transaction) error
+}
 
-// type EthersSigner interface {
-// 	// Ether wallet receive address
-// 	Addresss(ctx context.Context, client *TrustClient) string
-// 	// SignTypedData implement eip712 sign ...
-// 	SignTypedData(ctx context.Context, client *TrustClient, typedData *ethersigner.TypedData) ([]byte, error)
-// 	// SignTransaction sign ether transaction
-// 	SignTransaction(ctx context.Context, client *TrustClient, tx *ethersigner.Transaction) error
-// }
+// TenancyWallet hold signer generate wallet inforamtion
+type TenancyWallet struct {
+	KeyID     string            // wallet binding crypto provider key
+	BIP44Path string            // wallet bip44 path
+	Addresses map[string]string // wallet addresses
+}
+
+type Tenancy struct {
+	ID           string          // user id
+	WalletHot    *TenancyWallet  // hot user wallets
+	WalletBackup *TenancyWallet  // backup wallet
+	WalletCold   WalletAddresses //  cold wallet addresses
+	CreatedTime  time.Time       // tenancy created time
+	UpdatedTime  time.Time       // tenancy last update time
+}
+
+type WalletAddresses map[string]string
+
+type MultiTenancyProvider interface {
+	// create new user, and Generate wallets
+	New(ctx context.Context, walletCold WalletAddresses, adminPassword string) (userID string, err error)
+	// remove user
+	Delete(ctx context.Context, userID string) error
+	// Get wallet data
+	User(ctx context.Context, userID string) (*Tenancy, error)
+	// Regenerate signer manager wallet keys
+	RefreshWallet(ctx context.Context, userID string, adminPassword string) (*Tenancy, error)
+	// Update cold wallet
+	UpdateColdWallet(ctx context.Context, userID string, walletCold WalletAddresses) error
+}
+
+type MultiTenancyStorage interface {
+	New(ctx context.Context, tenancy *Tenancy) error
+	Delete(ctx context.Context, id string) error
+	Get(ctx context.Context, id string) (*Tenancy, error)
+	Update(ctx context.Context, id string, txF func(ctx context.Context, tenancy *Tenancy, updateF func(tenancy *Tenancy) error) error) error
+}
+
+type WalletProvider interface {
+	NewBIP44Wallet(ctx context.Context, id string, internal bool, adminPassword string) (path string, password string, err error)
+	Password(ctx context.Context, id string, bip44path string)
+}
 
 type CryptoProvider interface {
 	// if return empty string "", call CreateHDWallet to initialize crypto provider
@@ -38,7 +77,6 @@ type CryptoProvider interface {
 	CreateHDWallet(ctx context.Context, adminPassword string) (id string, err error)
 	// Delete HDWallet by id
 	DeleteHDWallet(ctx context.Context, adminPassword string) error
-
 	// Create or return exists drived wallet publickey and save private key protected by password to keystore
 	OpenKey(ctx context.Context, adminPassword string, sessionPassword string, bip44Path string, curves []elliptic.Curve) (string, [][]byte, error)
 	// Sign data with bip44 drived wallet
@@ -48,6 +86,8 @@ type CryptoProvider interface {
 	// Get Key public key
 	KeyPublicKey(ctx context.Context, keyID string, password string, curves []elliptic.Curve) ([][]byte, error)
 }
+
+// KeyStore ....
 
 type KeyStoreOps struct {
 	Force   bool
